e150e7daf019c804f610083c5b6d8617044a8c26
Cezare_Borgia <yndx-petrov-dmit@yandex.ru>
2023-10-15 16:40:22 +0400

    refactor: добавить проверку на нул, добавить стресстесты

--- src/main/java/ru/test/t1/service/ServiceT1.java
+++ src/main/java/ru/test/t1/service/ServiceT1.java
@@ -8,10 +8,14 @@ import java.util.*;
 public class ServiceT1 {
 
     public List<Map.Entry<Character, Integer>> countCharacter(String string) {
+        if (string == null) {
+            return Collections.emptyList();
+        }
         HashMap<Character, Integer> unsortedResult = new HashMap<>();
         for (Character character : string.toCharArray()) {
             unsortedResult.put(character, unsortedResult.getOrDefault(character, 0) + 1);
         }
+        // Deleted because a newline creates 2 characters at once "\n" and "\r"
         unsortedResult.remove("\r".charAt(0));
         List<Map.Entry<Character, Integer>> sortedResult = new ArrayList<>(unsortedResult.entrySet());
         sortedResult.sort((a, b) -> b.getValue().compareTo(a.getValue()));
